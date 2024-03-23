cc587765930afdcb3c76d9986d8f24610834ee39
Agnes Iradukunda <kundaaggy@gmail.com>
2024-03-23 19:36:04 +0200

    working on the styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import AirbnbLogo from '/airbnb.png'
 
 export default function Navbar(){
     return(
-        <nav className=''>
-           <img src={AirbnbLogo} alt="airbnb"/>
+        <nav className=' py-2 px-4 border shadow-md w-full'>
+           <img className='w-32 h-12' src={AirbnbLogo} alt="airbnb"/>
         </nav>
     )
 }
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,8 @@
 import heroImage from '/Group 77.jpg';
 export default function Hero() {
   return (
-    <div >
-        <img className='t w-min' src={heroImage} alt=""  />
+    <div  className='flex flex-col gap-12 items-center py-8'>
+        <img className='w-min' src={heroImage} alt=""  />
         <h1 className='text-4xl font-bold'>Online Experiences</h1>
         <p>Join unique interactive activities led by one-of-a-kind 
             hosts—all without leaving home.</p>
