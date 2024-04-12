f8708bc8d3628b0ca42f36edf1cabe0f54a397e1
Agnes Iradukunda <kundaaggy@gmail.com>
2024-04-12 20:02:00 +0200

    working on the card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import Navbar from "./components/Navbar"
+import Card from "./components/card"
 import Hero from "./components/hero"
+import data from "./data"
 
 function App() {
 
-
+ const cards = data.map((element) => {
+  return (
+    <Card
+    key={element}
+    img = {element.coverImg}
+    rating = {element.stats.rating}
+    reviewCount = {element.stats.reviewCount}
+    country = {element.location}
+    title = {element.description}
+    price = {element.price}
+  />
+  )
+ })
   return (
     <>
     <div>
       <Navbar/>
       <Hero/>
+      <div className="flex justify-center gap-6 px-24 py-8">
+        {cards}</div>
       </div>
     </>
   )
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
-export default [
+export default[
   {
     id: 1,
     title: "Life Lessons with Katie Zaferes",
     description:
       'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
     price: 136,
-    coverImg: "katie-zaferes.png",
+    coverImg: "https://a0.muscache.com/im/pictures/mediaverse/A4RE-PLP/original/88139d34-f308-43fc-908f-a07b893d794b.jpeg?im_w=1200&im_q=highq",
     stats: {
       rating: 5.0,
       reviewCount: 6,
@@ -19,7 +19,7 @@ export default [
     description:
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
-    coverImg: "wedding-photography.png",
+    coverImg: "https://a0.muscache.com/im/pictures/mediaverse/A4RE-PLP/original/46faca6b-df8a-4b76-9d45-d8bac7151141.jpeg?im_w=1200&im_q=highq",
     stats: {
       rating: 5.0,
       reviewCount: 30,
@@ -33,7 +33,7 @@ export default [
     description:
       "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
     price: 50,
-    coverImg: "mountain-bike.png",
+    coverImg: "https://a0.muscache.com/im/pictures/mediaverse/A4RE-PLP/original/2dbad1eb-dc50-4ed4-b796-ebcf2b236139.jpeg?im_w=1200&im_q=highq",
     stats: {
       rating: 4.8,
       reviewCount: 2,
